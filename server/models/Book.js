ea8da810c0aa85cbee0b243132482c717a5dcec1
mattias <mattias@stockholmscience.se>
2018-03-30 21:18:41 +0200

    added schema for book

---
+++ server/models/Book.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose')
+
+let BookSchema = new mongoose.Schema({
+  title: {type: String, required: true},
+  author: {type: String, required: true},
+  publishedYear: String,
+  isbn: String,
+  picture: String,
+  comments: [{body: String, date: {type: Date, default: Date.now}}],
+  mjaus: Number,
+  dateAdded: {type: Date, default: Date.now}
+})
+
+let Book = mongoose.model('Book', BookSchema)
+module.exports = Book
