8c6b66c368f50015c0c806a52d6a3a4f0d1d47bf
mattias <mattias@stockholmscience.se>
2018-03-31 22:09:51 +0200

    changed relative paths of bootstrap

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,12 +2,11 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
 import Main from '@/components/Main'
+import '../../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(Router)
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default new Router({
   routes: [
