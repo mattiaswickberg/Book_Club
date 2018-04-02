9bc8397deb88729f7926b37a0cd0d088aef55347
mattias <mattias@stockholmscience.se>
2018-04-02 14:55:38 +0200

    setting up database

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,1 +1,21 @@
 // import mongoose and start database connection
+var mongoose = require('mongoose')
+
+module.exports = {
+  initialize: function () {
+    var db = mongoose.connection
+
+// Handle connection error with database
+    db.on('error', function () {
+      console.log('We got a connection error')
+    })
+
+  // What happens database is connected
+    db.once('open', function () {
+      console.log('Database connected')
+    })
+
+  // Connect to database
+    mongoose.connect(process.env.DB_ADDRESS)
+  }
+}
--- server/server.js
+++ server/server.js
@@ -5,11 +5,25 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 const app = express()
+
+// Middleware
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(require('cookie-parser')(process.env.COOKIE_SECRET))
+
+// Start database server
+require('./lib/db').initialize()
+
+// Set CSP
+app.use(function (req, res, next) {
+  res.setHeader('Content-Security-Policy', 'script-src "self"')
+  return next()
+})
 
 app.get('/', function (req, res) {
   res.send('We all float down here')
