08acb735c2c3cfb7a356fa1b08c40ac102fe89d9
mattias <mattias@stockholmscience.se>
2018-04-04 14:24:24 +0200

    servering correct page

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -3,9 +3,9 @@ let User = require('../models/User')
 let createAccount = require('../lib/createAccount')
 // Main routes
 module.exports = function (app) {
-  app.get('/', function (req, res) {
+/*   app.get('/', function (req, res) {
     res.send('We all float down here')
-  })
+  } )*/
 
   // Account creation and login routes
   app.post('/createaccount', function (req, res) {
