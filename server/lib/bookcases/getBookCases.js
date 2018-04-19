0447721ae00fa5917c5b7f7dd1102e951061e857
mattias <mattias@stockholmscience.se>
2018-04-19 21:58:07 +0200

    fixed null in bookcases

--- server/lib/bookcases/getBookCases.js
+++ server/lib/bookcases/getBookCases.js
@@ -5,7 +5,17 @@ module.exports = function (userID) {
       resolve('not a valid userID')
     } else {
       Bookcase.find({user: userID}).then(response => {
-        resolve(response)
+        console.log(response)
+        let cases = []
+        response.forEach(element => {
+          let books = element.books.filter(function (item) {
+            return item !== null
+          })
+          element.books = books
+          cases.push(element)
+        })
+        console.log(cases)
+        resolve(cases)
       })
     }
   })
